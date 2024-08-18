37ac68b0e2754d300204ec717f9013fd881014ed
Vadim <vadimkalinin1703@gmail.com>
2024-08-18 15:00:59 +0300

    [KAN-29] [SpaceApp]: Прогресс реализации Главного экрана

--- SpaceProject/Modules/Main/View/MainView.swift
+++ SpaceProject/Modules/Main/View/MainView.swift
@@ -2,7 +2,7 @@
 //  MainView.swift
 //  SpaceProject
 //
-//  Created by Захар Литвинчук on 10.08.2024.
+//  Created by Zахар ЛитVинчук on 10.08.2024.
 //
 
 import UIKit
@@ -10,14 +10,26 @@ import SnapKit
 
 final class MainView: UIView {
 	// MARK: - UI Components
+    let backgroundImageView = UIImageView()
+    let bottomPageControl = UIPageControl()
+    let rocketInfoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    // FIXME: Change types location
+    enum SectionType: Int, CaseIterable {
+        case specificationInfo
+        case additionalInfo
+        case firstStageInfo
+        case secondStageInfo
+    }
 
-	let baseLabel = UILabel()
-
+    // MARK: - View initializator
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+        
 		embedViews()
 		setupAppearance()
 		setupLayout()
+        setupCollectionViewLayout()
 		setupBehavior()
 		setupData()
 	}
@@ -34,35 +46,133 @@ private extension MainView {
 	// MARK: - Embed Views
 
 	func embedViews() {
-		addSubviews(baseLabel)
+        addSubviews(
+            backgroundImageView,
+            bottomPageControl,
+            rocketInfoCollectionView
+        )
 	}
 
 	// MARK: - Setup Appearance
 
 	func setupAppearance() {
-		backgroundColor = .cyan
-
-		baseLabel.font = .systemFont(ofSize: 30, weight: .semibold)
-		baseLabel.textColor = .black
-		baseLabel.textAlignment = .center
-	}
+        // TODO: Add color switch by theme
+        backgroundImageView.backgroundColor = SpaceAppColor.backgroundSecondary.lightVariant
+        
+        bottomPageControl.backgroundColor = SpaceAppColor.backgroundSecondary.darkVariant
+        bottomPageControl.numberOfPages = 1
+        
+        rocketInfoCollectionView.backgroundColor = SpaceAppColor.background.darkVariant
+        rocketInfoCollectionView.layer.cornerRadius = 30
+        rocketInfoCollectionView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]    }
 
 	// MARK: - Setup Layout
 
 	func setupLayout() {
-		baseLabel.snp.makeConstraints {
-			$0.center.equalToSuperview()
-		}
+        backgroundImageView.snp.makeConstraints {
+            $0.horizontalEdges.top.equalToSuperview()
+            $0.height.equalTo(300)
+        }
+        bottomPageControl.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+        rocketInfoCollectionView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(bottomPageControl.snp.top)
+            $0.top.equalTo(backgroundImageView.snp.bottom).offset(-30)
+        }
 	}
 
 	// MARK: - Setup Behavior
 
 	func setupBehavior() {
-
+        rocketInfoCollectionView.register(
+            RocketCollectionVerticalCell.self,
+            forCellWithReuseIdentifier: RocketCollectionVerticalCell.identifier
+        )
+        rocketInfoCollectionView.register(
+            RocketCollectionHorizontalCell.self,
+            forCellWithReuseIdentifier: RocketCollectionHorizontalCell.identifier
+        )
 	}
 
 	// MARK: - Setup Data
 
 	func setupData() {
 	}
+    
+    // MARK: - Setup CollectionView Layout
+    
+    func setupCollectionViewLayout() {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvoirment) -> NSCollectionLayoutSection? in
+            guard let sectionType = SectionType(rawValue: sectionIndex) else {
+                return nil
+            }
+            
+            // MARK: Item layout
+            
+            let itemLayout = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalHeight(1)
+            )
+            let item = NSCollectionLayoutItem(layoutSize: itemLayout)
+            item.contentInsets = .init(top: 5, leading: 5, bottom: 5, trailing: 5)
+            
+            // MARK: Group layout
+            
+            let group: NSCollectionLayoutGroup
+            switch sectionType {
+            case .specificationInfo:
+                let groupLayout = NSCollectionLayoutSize(
+                    widthDimension: .absolute(110),
+                    heightDimension: .absolute(110)
+                )
+                group = NSCollectionLayoutGroup.horizontal(layoutSize: groupLayout, subitems: [item])
+            default:
+                let groupLayout = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(50)
+                )
+                group = NSCollectionLayoutGroup.vertical(layoutSize: groupLayout, subitems: [item])
+            }
+            
+            // MARK: Header and Footer views layout
+            
+            let headerFooterSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .absolute(100)
+            )
+            
+            // MARK: Section layout
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = .init(top: 10, leading: 30, bottom: 10, trailing: 30)
+            
+            // TODO: Add sections text headers
+            switch sectionIndex {
+            case 0:
+                let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerFooterSize,
+                    elementKind: UICollectionView.elementKindSectionHeader,
+                    alignment: .top
+                )
+                section.boundarySupplementaryItems = [sectionHeader]
+                section.orthogonalScrollingBehavior = .continuous
+            case (SectionType.allCases.count - 1):
+                let sectionFooter = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerFooterSize,
+                    elementKind: UICollectionView.elementKindSectionFooter,
+                    alignment: .bottom
+                )
+                section.boundarySupplementaryItems = [sectionFooter]
+            default:
+                section.boundarySupplementaryItems = []
+            }
+            
+            return section
+        }
+        
+        rocketInfoCollectionView.collectionViewLayout = layout
+    }
 }
