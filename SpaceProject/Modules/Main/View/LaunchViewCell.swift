c574c1978f201faa52e10510cf40734123cf1984
nklns <145385401+nklns@users.noreply.github.com>
2024-08-20 15:42:36 +0500

    Skeleton is DONE

---
+++ SpaceProject/Modules/Main/View/LaunchViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LaunchViewCell.swift
+//  SpaceProject
+//
+//  Created by Станислав Никулин on 20.08.2024.
+//
+
+import UIKit
+
+class LaunchViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
