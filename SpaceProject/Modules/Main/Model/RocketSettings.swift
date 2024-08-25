d709e1c9e802c2f6d08b220b3581a98ea4773e63
nklns <145385401+nklns@users.noreply.github.com>
2024-08-25 08:36:36 +0500

    add documentation

--- SpaceProject/Modules/Main/Model/RocketSettings.swift
+++ SpaceProject/Modules/Main/Model/RocketSettings.swift
@@ -5,7 +5,11 @@
 //  Created by Станислав Никулин on 22.08.2024.
 //
 
+/// Тип измерения для параметров ракеты.
 enum TypeOfMeasurement {
+    /// Высота
     case height
+    
+    /// Вес
     case weight
 }
