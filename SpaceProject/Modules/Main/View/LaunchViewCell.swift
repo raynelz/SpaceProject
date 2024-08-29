6d0c2e2abb4a92a8d10c8b5b6a5d1d928997ff3d
nklns <145385401+nklns@users.noreply.github.com>
2024-08-29 21:34:02 +0500

    add to develop changes from branch KAN-45

---
+++ SpaceProject/Modules/Main/View/LaunchViewCell.swift
@@ -0,0 +1,90 @@
+//
+//  LaunchViewCell.swift
+//  SpaceProject
+//
+//  Created by Станислав Никулин on 20.08.2024.
+//
+
+import UIKit
+import SnapKit
+
+final class LaunchViewCell: UITableViewCell {
+    
+    // MARK: - UI Components
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .regular)
+        label.textColor = SpaceAppColor.text
+        return label
+    }()
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.textColor = SpaceAppColor.cellText
+        return label
+    }()
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupLayout()
+        setupCellAppearance()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configuration
+    func configure(model: Launch) {
+        nameLabel.text = model.name
+        dateLabel.text = model.date
+        iconImageView.image = model.launchIs ? UIImage(named: "rocket-done") :
+        UIImage(named: "rocket-fail")
+    }
+}
+
+// MARK: - Private Methods
+
+private extension LaunchViewCell {
+    
+    // MARK: - Setup Views
+    func setupViews() {
+        contentView.addSubviews(nameLabel, dateLabel, iconImageView)
+    }
+    
+    // MARK: - Setup Appearance
+    func setupCellAppearance() {
+        backgroundColor = .clear
+        contentView.backgroundColor = SpaceAppColor.cellBackground
+        contentView.layer.cornerRadius = 20
+        contentView.layer.masksToBounds = true
+        // Selected Cell
+        let selectedBackgroundColor = UIView()
+        selectedBackgroundColor.backgroundColor = SpaceAppColor.cellBackground
+        self.selectedBackgroundView = selectedBackgroundColor
+        self.selectedBackgroundView?.layer.cornerRadius = 20
+    }
+    
+    // MARK: - Setup Layout
+    func setupLayout() {
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalToSuperview().inset(25)
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.leading.equalTo(nameLabel.snp.leading)
+            make.bottom.equalToSuperview().inset(25)
+        }
+        iconImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+    }
+}
