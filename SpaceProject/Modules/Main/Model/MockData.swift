87140bf5e35813cfbca6edcc4ad1992fc809bc1d
nklns <145385401+nklns@users.noreply.github.com>
2024-08-29 21:42:39 +0500

    added Launch Mock and return original of MockData

--- SpaceProject/Modules/Main/Model/MockData.swift
+++ SpaceProject/Modules/Main/Model/MockData.swift
@@ -75,17 +75,3 @@ enum MockData {
         )
     ]
 }
-
-
-struct LaunchMockTest {
-    static let launches: [Launch] =
-    [
-        Launch(name: "Falcon-9", date: "12 august 2019", launchIs: true),
-        Launch(name: "Nasa Space Ship", date: "28 september 2020", launchIs: false),
-        Launch(name: "Bobir kurwa", date: "30 august 2004", launchIs: true),
-        Launch(name: "Bobir kurwa", date: "30 august 2004", launchIs: true),
-        Launch(name: "Bobir kurwa", date: "30 august 2004", launchIs: true),
-        Launch(name: "Bobir kurwa", date: "30 august 2004", launchIs: true),
-        Launch(name: "Bobir kurwa", date: "30 august 2004", launchIs: true)
-    ]
-}
