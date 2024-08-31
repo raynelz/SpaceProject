fc61402c2b5be91af374a42ba135a46fcf7e2ef8
nklns <145385401+nklns@users.noreply.github.com>
2024-08-31 13:22:45 +0500

    turn to final stage KAN-53 to merge into develop

---
+++ SpaceProject/Modules/Main/Model/RocketSettingsModel.swift
@@ -0,0 +1,15 @@
+//
+//  RocketSettings.swift
+//  SpaceProject
+//
+//  Created by Станислав Никулин on 22.08.2024.
+//
+
+/// Тип измерения для параметров ракеты.
+enum TypeOfMeasurement {
+    /// Высота
+    case height
+    
+    /// Вес
+    case weight
+}
--- SpaceProject/Resources/Colors.swift
+++ SpaceProject/Resources/Colors.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-/// Цветовая тема приложения
+/// Перечисление `SpaceAppColor` содержит статические свойства, представляющие цвета, используемые в приложении.
+/// Эти цвета задают внешний вид различных элементов пользовательского интерфейса.
 enum SpaceAppColor {
+    /// Цвет для вторичных фонов.
     static let backgroundSecondary = UIColor(named: "BackgroundSecondaryColor")
+    
+    /// Цвет фона ячеек.
     static let cellBackground = UIColor(named: "CellBackgroundColor")
+    
+    /// Цвет для вторичных фонов ячеек.
+    static let cellBackgroundSecondary = UIColor(named: "CellBackgroundSecondary")
+    
+    /// Основной фоновый цвет приложения.
     static let background = UIColor.systemBackground
-    static let textSecondary = UIColor(named: "TextSecondaryColor")
-    static let cellText = UIColor(named: "CellTextColor")
+    
+    /// Основной цвет текста.
     static let text = UIColor.label
+    
+    /// Цвет текста для вторичных элементов.
+    static let textSecondary = UIColor(named: "TextSecondaryColor")
+    
+    /// Цвет текста в ячейках.
+    static let cellText = UIColor(named: "CellTextColor")
+    
+    /// Вторичный цвет текста в ячейках.
+    static let cellTextSeconary = UIColor(named: "CellTextColorSecondary")
 }
+
